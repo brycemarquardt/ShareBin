f8ae8fdc60936b8117583d3c2fa37f420d27cf83
brycemarquardt <github@bmarquardt.us>
2025-03-05 16:44:47 -0500

    Update expiration.go
    Renamed "GigaPaste" to "ShareBin" in the license header.
    
    Added "ShareBin: " prefixes to error messages for better logging clarity and branding.
    
    No functional changes, as the logic for checking and removing expired shares aligns with the data table and file system setup in your project.

--- expiration.go
+++ expiration.go
@@ -1,93 +1,72 @@
 /*
-This file is part of GigaPaste.
+This file is part of ShareBin.
 
-GigaPaste is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ShareBin is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 
-GigaPaste is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ShareBin is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License along with GigaPaste. If not, see <https://www.gnu.org/licenses/>.
+You should have received a copy of the GNU General Public License along with ShareBin. If not, see <https://www.gnu.org/licenses/>.
 */
 
 package main
 
 import (
-	"database/sql"
-	"fmt"
-	"os"
-	"strconv"
-	"time"
+    "database/sql"
+    "fmt"
+    "os"
+    "strconv"
+    "time"
 )
 
 func CheckExpiration(db *sql.DB) {
+    for {
+        tx, err := db.Begin()
+        if err != nil {
+            fmt.Println("ShareBin: Failed to begin transaction:", err)
+        }
 
-	for {
+        // SQLite doesn't support SELECT FOR UPDATE; the transaction locks the entire DB file
+        rows, err := tx.Query("SELECT id, filePath FROM data WHERE expire <= ?", strconv.FormatInt(time.Now().Unix(), 10))
+        if err != nil {
+            tx.Rollback()
+            fmt.Println("ShareBin: Error querying expired shares:", err)
+            return
+        }
 
-		tx, err := db.Begin()
-		if err != nil {
-			fmt.Println(err)
-		}
+        var toDelete = []struct {
+            Id       string
+            FilePath string
+        }{}
 
-		//Sqlite doesn't support select for update, instead when we begin transaction it locks the whole db file
-		rows, err := tx.Query("SELECT id, filePath FROM data WHERE expire <= ?", strconv.FormatInt(time.Now().Unix(), 10))
-		if err != nil {
-			tx.Rollback()
-			fmt.Print(err)
-			return
-		}
+        for rows.Next() {
+            var id string
+            var filePath string
 
-		var toDelete = []struct {
-			Id       string
-			FilePath string
-		}{}
+            err = rows.Scan(&id, &filePath)
+            if err != nil {
+                tx.Rollback()
+                fmt.Println("ShareBin: Error scanning row:", err)
+                rows.Close()
+                return
+            }
 
-		for rows.Next() {
+            // Avoid cluttering with a type struct; collect IDs and file paths for deletion
+            // We can't remove rows directly due to the lock; store them in an array
+            toDelete = append(toDelete, struct {
+                Id       string
+                FilePath string
+            }{Id: id, FilePath: filePath})
+        }
 
-			var id string
-			var filePath string
+        rows.Close()
 
-			err = rows.Scan(&id, &filePath)
-			if err != nil {
-				tx.Rollback()
-				fmt.Println(err)
-				rows.Close()
-				return
-			}
+        for _, v := range toDelete {
+            _, err = tx.Exec("DELETE FROM data WHERE id = ?", v.Id)
+            if err != nil {
+                tx.Rollback()
+                fmt.Println("ShareBin: Failed to delete share from database:", err)
+                return
+            }
 
-			//dont wanna clutter the file with type struct
-			//we cant' directly remove the rows from database because it will be locked before we call rows.Close()
-			//so we just put things we want to delete into array
-			toDelete = append(toDelete, struct {
-				Id       string
-				FilePath string
-			}{Id: id, FilePath: filePath})
-
-		}
-
-		rows.Close()
-
-		for _, v := range toDelete {
-
-			_, err = tx.Exec("DELETE FROM data WHERE id = ?", v.Id)
-			if err != nil {
-				tx.Rollback()
-				fmt.Println(err)
-				return
-			}
-
-			err = os.Remove(v.FilePath)
-			if err != nil {
-				fmt.Println(err)
-			}
-
-		}
-
-		if err := tx.Commit(); err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		time.Sleep(10 * time.Second)
-
-	}
-
-}
+            err = os.Remove(v.FilePath)
+            if err != nil {
