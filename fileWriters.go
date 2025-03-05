c97ea3df38e2482109786dcf3a3e0c706bc5c43d
brycemarquardt <github@bmarquardt.us>
2025-03-05 16:57:54 -0500

    Update fileWriters.go
    Renamed "GigaPaste" to "ShareBin" in the license header.
    
    Added "ShareBin: " prefixes to error messages for better logging clarity and branding.
    
    No functional changes, as the file writing logic (MultipleFileWriter for zipping multiple files, SingleFileWriter for single files) is already used correctly in handlers.go.

--- fileWriters.go
+++ fileWriters.go
@@ -1,150 +1,115 @@
 /*
-This file is part of GigaPaste.
+This file is part of ShareBin.
 
-GigaPaste is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ShareBin is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 
-GigaPaste is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ShareBin is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License along with GigaPaste. If not, see <https://www.gnu.org/licenses/>.
+You should have received a copy of the GNU General Public License along with ShareBin. If not, see <https://www.gnu.org/licenses/>.
 */
 
 package main
 
 import (
-	"archive/zip"
-	"fmt"
-	"io"
-	"mime/multipart"
-	"os"
-	"time"
+    "archive/zip"
+    "fmt"
+    "io"
+    "mime/multipart"
+    "os"
+    "time"
 )
 
 func MultipleFileWriter(files []*multipart.FileHeader, path string, aesKey []byte, callback func()) {
+    outFile, err := os.Create(path)
+    if err != nil {
+        fmt.Println("ShareBin: Failed to create multiple file output:", err)
+        return
+    }
+    defer outFile.Close()
 
-	outFile, err := os.Create(path)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer outFile.Close()
+    // Make new zip file
+    zipWriter := zip.NewWriter(outFile)
+    defer zipWriter.Close()
 
-	//make new zip file
-	zipWriter := zip.NewWriter(outFile)
-	defer zipWriter.Close()
+    for _, fileHeader := range files {
+        file, err := fileHeader.Open()
+        if err != nil {
+            fmt.Println("ShareBin: Failed to open multiple file:", err)
+            return
+        }
+        defer file.Close()
 
-	for _, fileHeader := range files {
+        // Create file inside the zip
+        writer, err := zipWriter.Create(fileHeader.Filename)
+        if err != nil {
+            fmt.Println("ShareBin: Failed to create zip entry:", err)
+            return
+        }
 
-		file, err := fileHeader.Open()
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		defer file.Close()
+        buffer := make([]byte, 1024*Global.StreamSizeLimit)
+        for {
+            n, err := file.Read(buffer)
+            if err != nil && err != io.EOF {
+                fmt.Println("ShareBin: Error reading multiple file chunk:", err)
+                return
+            }
+            if n == 0 {
+                break
+            }
 
-		//create file inside the zip
-		writer, err := zipWriter.Create(fileHeader.Filename)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
+            // Write the chunk to the ZIP file
+            _, err = writer.Write(buffer[:n])
+            if err != nil {
+                fmt.Println("ShareBin: Failed to write to zip:", err)
+                return
+            }
 
-		buffer := make([]byte, 1024*Global.StreamSizeLimit)
-		for {
+            // Need to add check for > 0 because Sleep(0) will just trigger context switch
+            if Global.StreamThrottle > 0 {
+                time.Sleep(time.Duration(Global.StreamThrottle) * time.Millisecond)
+            }
+        }
+    }
 
-			n, err := file.Read(buffer)
-			if err != nil && err != io.EOF {
-				fmt.Println(err)
-				return
-			}
-			if n == 0 {
-				break
-			}
+    zipWriter.Close()
+    outFile.Close()
 
-			// Write the chunk to the ZIP file
-			_, err = writer.Write(buffer[:n])
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
+    if aesKey != nil {
+        err = EncryptFile(path, aesKey)
+        if err != nil {
+            fmt.Println("ShareBin: Failed to encrypt multiple file ZIP:", err)
+        }
+    }
 
-			//need to add check for > 0 because Sleep(0) will just trigger context switch
-			if Global.StreamThrottle > 0 {
-				time.Sleep(time.Duration(Global.StreamThrottle) * time.Millisecond)
-			}
-
-		}
-
-	}
-
-	zipWriter.Close()
-	outFile.Close()
-
-	if aesKey != nil {
-
-		err = EncryptFile(path, aesKey)
-		if err != nil {
-			fmt.Println(err)
-		}
-
-	}
-
-	callback()
-
+    callback()
 }
 
 func SingleFileWriter(files []*multipart.FileHeader, path string, aesKey []byte, callback func()) {
+    outFile, err := os.Create(path)
+    if err != nil {
+        fmt.Println("ShareBin: Failed to create single file output:", err)
+        return
+    }
+    defer outFile.Close()
 
-	outFile, err := os.Create(path)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer outFile.Close()
+    file, err := files[0].Open()
+    if err != nil {
+        fmt.Println("ShareBin: Failed to open single file:", err)
+        return
+    }
+    defer file.Close()
 
-	file, err := files[0].Open()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+    // Use a buffered reader to read the file in chunks
+    buffer := make([]byte, 1024*Global.StreamSizeLimit)
+    for {
+        n, err := file.Read(buffer)
+        if err != nil && err != io.EOF {
+            fmt.Println("ShareBin: Error reading single file chunk:", err)
+            return
+        }
+        if n == 0 {
+            break
+        }
 
-	defer file.Close()
-
-	// Use a buffered reader to read the file in chunks
-	buffer := make([]byte, 1024*Global.StreamSizeLimit)
-	for {
-		n, err := file.Read(buffer)
-		if err != nil && err != io.EOF {
-			fmt.Println(err)
-			return
-		}
-		if n == 0 {
-			break
-		}
-
-		// Write the chunk to the output file
-		_, err = outFile.Write(buffer[:n])
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		//need to add check for > 0 because Sleep(0) will just trigger context switch
-		if Global.StreamThrottle > 0 {
-			time.Sleep(time.Duration(Global.StreamThrottle) * time.Millisecond)
-		}
-
-	}
-
-	outFile.Close()
-	if aesKey != nil {
-
-		err = EncryptFile(path, aesKey)
-		if err != nil {
-			fmt.Println(err)
-		}
-
-	}
-
-	callback()
-
-}
+        // Write the chunk to the output file
+        _, err = outFile
